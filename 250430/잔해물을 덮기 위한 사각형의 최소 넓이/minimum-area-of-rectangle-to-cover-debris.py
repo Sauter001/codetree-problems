32f7020fb278e8097b8ff8f5f157f13a41eb200f
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-04-30 15:37:40 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 47ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250430/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250430/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -7,6 +7,10 @@ x1[1], y1[1], x2[1], y2[1] = map(int, input().split())
 
 # Please write your code here.
 width, height = 0, 0
+if (x2[1] <= x1[0]) or (x2[0] <= x1[1]) or (y2[1] <= y1[0]) or (y2[0] <= y1[1]):
+        width = x2[0] - x1[0]
+        height = y2[0] - y1[0]
+
 if (x1[0] < x1[1] < x2[0]) or (x1[0] < x2[1] < x2[0]):
     if (y1[0] < y2[1] < y2[0]) or (y1[0] < y1[1] < y2[0]):
         width = x2[0] - x1[0]
@@ -20,8 +24,5 @@ elif (x1[1] <= x1[0] <= x2[1]) and (x1[1] <= x2[0] <= x2[1]):
         height = (y1[1] - y1[0]) 
     elif (y1[0] < y2[1] < y2[0]):
         height = (y2[0] - y2[1])
-elif (x2[1] <= x1[0]) or (x2[0] <= x1[1]) or (y2[1] <= y1[0]) or (y2[0] <= y1[1]):
-        width = x2[0] - x1[0]
-        height = y2[0] - y1[0]
 
 print(width * height)
