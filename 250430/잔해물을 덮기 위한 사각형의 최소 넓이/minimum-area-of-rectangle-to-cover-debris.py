76aa8a495692c4d82217353dd55d5e4288304fac
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-04-30 15:51:35 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250430/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 250430/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -10,26 +10,26 @@ width, height = 0, 0
 if (x2[1] <= x1[0]) or (x2[0] <= x1[1]) or (y2[1] <= y1[0]) or (y2[0] <= y1[1]):
         width = x2[0] - x1[0]
         height = y2[0] - y1[0]
-
-if (x1[0] < x1[1] < x2[0]) or (x1[0] < x2[1] < x2[0]):
-    if (y1[0] < y2[1] < y2[0]) or (y1[0] < y1[1] < y2[0]):
-        width = x2[0] - x1[0]
-        height = y2[0] - y1[0]
-    elif (y1[1] <= y1[0] <= y2[1]) and (y1[1] <= y2[0] <= y2[1]):
-        if (x1[0] < x1[1] < x2[0]) and (x2[0] <= x2[1]): 
-            width = (x1[1] - x1[0])  
-        elif (x1[0] < x2[1] < x2[0]) and (x1[1] <= x1[0]):
-            width = (x2[0] - x2[1])
-        else: 
+else:
+    if (x1[0] < x1[1] < x2[0]) or (x1[0] < x2[1] < x2[0]):
+        if (y1[0] < y2[1] < y2[0]) or (y1[0] < y1[1] < y2[0]):
             width = x2[0] - x1[0]
-        height = y2[0] - y1[0]
-elif (x1[1] <= x1[0] <= x2[1]) and (x1[1] <= x2[0] <= x2[1]):
-    width = x2[0] - x1[0]
-    if (y1[0] < y1[1] < y2[0]) and (y2[0] <= y2[1]) :
-        height = (y1[1] - y1[0]) 
-    elif (y1[0] < y2[1] < y2[0]) and (y1[1] <= y1[0]):
-        height = (y2[0] - y2[1])
-    else:
-        height = y2[0] - y1[0]
+            height = y2[0] - y1[0]
+        elif (y1[1] <= y1[0] <= y2[1]) and (y1[1] <= y2[0] <= y2[1]):
+            if (x1[0] < x1[1] < x2[0]) and (x2[0] <= x2[1]): 
+                width = (x1[1] - x1[0])  
+            elif (x1[0] < x2[1] < x2[0]) and (x1[1] <= x1[0]):
+                width = (x2[0] - x2[1])
+            else: 
+                width = x2[0] - x1[0]
+            height = y2[0] - y1[0]
+    elif (x1[1] <= x1[0] <= x2[1]) and (x1[1] <= x2[0] <= x2[1]):
+        width = x2[0] - x1[0]
+        if (y1[0] < y1[1] < y2[0]) and (y2[0] <= y2[1]) :
+            height = (y1[1] - y1[0]) 
+        elif (y1[0] < y2[1] < y2[0]) and (y1[1] <= y1[0]):
+            height = (y2[0] - y2[1])
+        else:
+            height = y2[0] - y1[0]
 
 print(width * height)
