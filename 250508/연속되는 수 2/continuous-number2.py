736fd36131b432e08ccd1917c3d0b323fba8edbd
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-08 15:13:07 +0900

    Title: 연속되는 수 2, Time: 68ms, Memory: 16MB, Status: Passed - Codetree

--- 250508/연속되는 수 2/continuous-number2.py
+++ 250508/연속되는 수 2/continuous-number2.py
@@ -2,18 +2,13 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
-sequence_counts = []
-cnt = 0
+cnt, result = 0, 0
 
 for i in range(n):
-    if i == 0:
-        cnt += 1
-    elif arr[i] != arr[i - 1]:
-        sequence_counts.append(cnt)
+    if i == 0 or arr[i] != arr[i - 1]:
         cnt = 1
     else:
         cnt += 1
+    result = max(cnt, result)
 
-if not sequence_counts:
-    sequence_counts.append(cnt)
-print(max(sequence_counts))
+print(result)
