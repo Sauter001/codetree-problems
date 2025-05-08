1d3be6701eef49e50ea263eefac26bc95d6f66fa
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-08 15:05:34 +0900

    Title: 연속되는 수 2, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250508/연속되는 수 2/continuous-number2.py
+++ 250508/연속되는 수 2/continuous-number2.py
@@ -2,9 +2,15 @@ n = int(input())
 arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
+sequence_counts = []
 cnt = 0
 
 for i in range(n):
-    if i == 0 or arr[i] != arr[i - 1]:
+    if i == 0:
         cnt += 1
-print(cnt)
+    elif arr[i] != arr[i - 1]:
+        sequence_counts.append(cnt)
+        cnt = 1
+    else:
+        cnt += 1
+print(max(sequence_counts))
