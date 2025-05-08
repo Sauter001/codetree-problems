01471a3e4f9c2fad14b1d8550236236749e1a62d
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-09 02:26:44 +0900

    Title: 움직이는 블록, Time: 77ms, Memory: 18MB, Status: Passed - Codetree

--- 250509/움직이는 블록/moving-block.py
+++ 250509/움직이는 블록/moving-block.py
@@ -4,6 +4,6 @@ num_blocks = sum(blocks)
 
 # Please write your code here.
 blocks_per_pos = num_blocks // n
-blocks_to_move = list(map(lambda x: x - blocks_per_pos if x > blocks_per_pos else 0, blocks))
+blocks_to_move = [x - blocks_per_pos if x > blocks_per_pos else 0 for x in blocks]
 
 print(sum(blocks_to_move))
