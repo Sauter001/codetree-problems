209e1bed49b0b0da427d11c3a979cc05a14f9c89
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-10 12:38:41 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250510/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
+++ 250510/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.py
@@ -4,21 +4,23 @@ handshakes = [tuple(map(int, input().split())) for _ in range(T)]
 # Please write your code here.
 infected_list = [False] * (N + 1)
 infected_list[P] = True
-handshake_counts = {dev: 0 for dev in range(N + 1)}
+infected_times = [-1] * (N + 1)
+infected_times[P] = 0
+handshake_counts = [0] * (N + 1)
 handshakes.sort()
 
 # 감염시키는 사람은 전염됐다고 가정
-def infect_person(from_dev, to_dev):
+def infect_person(infected_time, from_dev, to_dev):
     if handshake_counts[from_dev] < K:
         infected_list[to_dev] = True
-        handshake_counts[from_dev] += 1     
+        infected_times[to_dev] = infected_time
+    handshake_counts[from_dev] += 1     
 
 for t, x, y in handshakes:
     # print(t,x,y)
-
-    if infected_list[x]:
-        infect_person(x, y)
-    if infected_list[y]:
-        infect_person(y, x)
+    for a, b in [(x, y), (y, x)]:
+        if infected_list[a] and infected_times[a] < t:
+            infect_person(t, a, b)
+    # print(handshake_counts[1:])
 
 print(''.join('1' if infected else '0' for infected in infected_list[1:]))
