dfb9cb5485333c02c3e8592e277d632f37c31baa
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-13 03:05:27 +0900

    Title: 체크판위에서 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250513/체크판위에서 2/on-the-checkboard-2.py
+++ 250513/체크판위에서 2/on-the-checkboard-2.py
@@ -5,9 +5,10 @@ grid = [list(input().split()) for _ in range(R)]
 result = 0
 for i in range(1, R):
     for j in range(1, C):
-        for k in range(i + 1, R):
-            for l in range(j + 1, C):
+        for k in range(i + 1, R - 1):
+            for l in range(j + 1, C - 1):
                 path = [grid[0][0], grid[i][j], grid[k][l], grid[-1][-1]]
+                # print(f"path = {path}, via ({i},{j}) → ({k},{l})")
                 if all(path[x] != path[x + 1] for x in range(len(path) - 1)):
                     result += 1
 print(result)
