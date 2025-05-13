7c8ea76c3045f048cf320a161f75a754a53a0f42
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-13 12:16:12 +0900

    Title: 마라톤 중간에 택시타기 2, Time: 82ms, Memory: 18MB, Status: Runtime Error - Codetree

--- 250513/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
+++ 250513/마라톤 중간에 택시타기 2/taking-a-taxi-in-the-middle-of-the-marathon-2.py
@@ -9,11 +9,9 @@ def distance(x1, y1, x2, y2):
 
 shortest_dist = 100 ** 6
 for skip_point in range(1, n-1):
-    path = [(0, 0)]
     for i in range(n):
         if i == skip_point: continue
         path.append((x[i], y[i]))
-    path.append((x[-1], y[-1]))
 
     distance_skipped = 0
     for i in range(len(path) - 1):
