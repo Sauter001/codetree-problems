485bd728b6212c2160eb7d2406dd2ab2330a0f00
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-16 00:56:25 +0900

    Title: Carry 피하기 2, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250516/Carry 피하기 2/escaping-carry-2.py
+++ 250516/Carry 피하기 2/escaping-carry-2.py
@@ -3,13 +3,12 @@ arr = [int(input()) for _ in range(n)]
 
 # Please write your code here.
 def check_carry(*nums):
-    digit = 10
     tmps = [x for x in nums]
-    while any(x % digit != x for x in tmps):
-        # print(tmps)
-        if sum(x % digit for x in tmps) >= 10:
+    while any(x > 0 for x in tmps):
+        digits = [x % 10 for x in tmps]
+        if sum(digits) >= 10:
             return 0
-        tmps = [x // digit for x in tmps]
+        tmps = [x // 10 for x in tmps]
     return sum(nums)
 
 res = 0
