d47e69307516d670b70ed2ab9b18a45ef6944887
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-05-16 01:40:03 +0900

    Title: 오목, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250516/오목/O-mok.py
@@ -0,0 +1,37 @@
+n = 19
+board = [list(map(int, input().split())) for _ in range(19)]
+
+# Please write your code here.
+result = 0
+winner_center = (-1, -1)
+
+# 가로 탐색
+for i in range(n):
+    for j in range(n - 5 + 1):
+        if board[i][j] == 0:
+            continue
+
+        is_five = True
+        for k in range(j, j + 4):
+            if board[i][k] != board[i][k + 1]:
+                is_five = False
+        if is_five:
+            result = board[i][j]
+            winner_center = (i + 1, j + 3)
+
+# 세로 탐색
+for i in range(n):
+    for j in range(n - 5 + 1):
+        if board[j][i] == 0:
+            continue
+
+        is_five = True
+        for k in range(j, j + 4):
+            if board[k][i] != board[k + 1][i]:
+                is_five = False
+        if is_five:
+            result = board[j][i]
+            winner_center = (j + 3, i + 1)
+
+print(result)
+print(*winner_center if winner_center != (-1, -1) else '')
