07a21e4907faa975a0223951befa48fe4defcfe7
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-06-29 02:16:22 +0900

    Title: 겹치지 않는 선분 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250629/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -0,0 +1,23 @@
+n = int(input())
+lines = [tuple(map(int, input().split())) for _ in range(n)]
+lines.sort()
+checks = [False] * n
+
+# Please write your code here.
+non_intersect_lines = 0
+def is_intersect(line1, line2):
+    # 기울기 같으면 교차 안 함
+    if line2[1] - line1[0] == line2[1] - line2[0]:
+        return False
+    return (line1[0] < line2[0]) and (line1[1] > line2[1])
+
+for i in range(n):
+    result = True
+    if checks[i]:
+        continue
+    for j in range(i + 1, n):
+        if is_intersect(lines[i], lines[j]):
+            checks[i] = checks[j] = True
+            result = False
+    non_intersect_lines += 1 if result else 0
+print(non_intersect_lines)
