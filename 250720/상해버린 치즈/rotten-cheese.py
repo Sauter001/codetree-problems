cfc7d684b7b5bd022666a9b26048ece1ce92910b
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-07-20 15:44:39 +0900

    Title: 상해버린 치즈, Time: 51ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250720/상해버린 치즈/rotten-cheese.py
+++ 250720/상해버린 치즈/rotten-cheese.py
@@ -25,7 +25,7 @@ for p, t in zip(sick_p, sick_t):
     # 아프기 전 먹은 치즈 조회
     for i in range(earliest_counter[p][0], t):
         # print(f"{p}, {t}: {i} => {who_eat[(p, i)]}")
-        rotten_cheese_candidates |= set(who_eat[(p, i)])
+        rotten_cheese_candidates &= set(who_eat[(p, i)])
 
 checker = [False] * (N + 1)
 for (p, t), cheeses in who_eat.items():
