ec8c8d5ff9d44503514b84f5d0e81a8830ad0c12
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-07-20 16:55:19 +0900

    Title: 흥미로운 숫자 2, Time: 258ms, Memory: 18MB, Status: Passed - Codetree

--- 250720/흥미로운 숫자 2/interesting-numbers-2.py
+++ 250720/흥미로운 숫자 2/interesting-numbers-2.py
@@ -1,6 +1,5 @@
 X, Y = map(int, input().split())
 
-# Please write your code here.
 def convert_to_digits(n):
     res = []
     divisor = 10
@@ -13,6 +12,7 @@ count = 0
 for x in range(X, Y+1):
     digits = convert_to_digits(x)
     digit_counter = {}
+
     for d in digits:
         if d not in digit_counter:
             digit_counter[d] = 0
@@ -21,7 +21,6 @@ for x in range(X, Y+1):
     one_count = 0
     for v in digit_counter.values():
         if v == 1: one_count += 1
-    
-    if one_count == 1:
+    if one_count == 1 and len(digit_counter) == 2:
         count += 1
 print(count)
