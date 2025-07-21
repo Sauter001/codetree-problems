75e20aec877b2061dc970f9e452f75e2aa1dbc02
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-07-21 14:19:40 +0900

    Title: 개발자의 순위, Time: 48ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/개발자의 순위/developers-rank.py
+++ 250721/개발자의 순위/developers-rank.py
@@ -5,14 +5,12 @@ prior_map = {}
 
 # 전처리
 for i in range(1, n + 1):
-    prior_map[i] = {range(1, n + 1)}
+    prior_map[i] = {x for x in range(1, n + 1)}
 
 for i in range(k):
     for j in range(n):
         rank_set = { arr[i][m] for m in range(j + 1, n) }
-        print(rank_set, i)
-        prior_map[j + 1] &= rank_set
+        prior_map[arr[i][j]] &= rank_set
 
-print(prior_map)
-
-
+res = sum(len(m) for m in prior_map.values())
+print(res)
