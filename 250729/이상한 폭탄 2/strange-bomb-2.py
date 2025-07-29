94716e98cd7d26e6ce6f796477fb4deba5c304e9
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-07-29 21:25:32 +0900

    Title: 이상한 폭탄 2, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250729/이상한 폭탄 2/strange-bomb-2.py
@@ -0,0 +1,12 @@
+N, K = map(int, input().split())
+nums = [int(input()) for _ in range(N)]
+bomb_nums = set(nums)
+
+res = 0
+for b in bomb_nums:
+    for i in range(N):
+        if nums[i] == b:
+            for j in range(i + 1, min(N, i + K + 1)):
+                if nums[i] == nums[j]:
+                    res = max(res, nums[i])
+print(res)        
