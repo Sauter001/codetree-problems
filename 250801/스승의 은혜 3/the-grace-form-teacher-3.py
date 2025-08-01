55e6e8a7a282f1634e405a5b87f9111e19628e6a
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-08-01 23:34:29 +0900

    Title: 스승의 은혜 3, Time: 114ms, Memory: 18MB, Status: Wrong Answer - Codetree

---
+++ 250801/스승의 은혜 3/the-grace-form-teacher-3.py
@@ -0,0 +1,21 @@
+n, budget = map(int, input().split())
+gifts = [tuple(map(int, input().split())) for _ in range(n)]
+res = 0
+for i in range(n):
+    price, sail = gifts[i]
+    gifts[i] = (price // 2, sail)
+    candidates = sorted(map(lambda x: x[0] + x[1], gifts))
+    
+    tb = budget
+    count = 0
+    for c in candidates:
+        tb -= c
+        if tb >= 0:
+            count += 1
+        else:
+            break
+    res = max(res, count)
+
+    gifts[i] *= 2 # 상태 복구
+
+print(res)
