a9e8c2afee1a0f9ffd66d21ea34804c7efb8e61c
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-08-03 22:55:39 +0900

    Title: 최소 경로로 탈출 하기, Time: 73ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250803/최소 경로로 탈출 하기/escape-with-min-distance.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+n, m = map(int, input().split())
+grid = [list(map(int, input().split())) for _ in range(n)]
+steps = [[0] * m for _ in range(n)]
+dxs, dys = (1, 0, -1, 0), (0, 1, 0, -1)
+
+count = -1
+visited = [[False] * m for _ in range(n)]
+q = deque([(0, 0)])
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < m
+
+def push(x, y, s):
+    steps[x][y] = s
+    visited[x][y] = True
+    q.append((x, y))
+
+push(0, 0, 0)
+while q:
+    x, y = q.popleft()
+
+    if visited[x][y]:
+        continue
+    for dx, dy in zip(dxs, dys):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and grid[nx][ny] == 1 and not visited[nx][ny]:
+            push(nx, ny, step[x][y] + 1)
+
+print(step[n - 1][n - 1])
+
