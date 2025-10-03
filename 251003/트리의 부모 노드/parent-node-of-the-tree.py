93efcd16d6afc2251e89b11b6e89bc34d4ce5727
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-10-03 17:06:06 +0900

    Title: 트리의 부모 노드, Time: 305ms, Memory: 44MB, Status: Passed - Codetree

--- 251003/트리의 부모 노드/parent-node-of-the-tree.py
+++ 251003/트리의 부모 노드/parent-node-of-the-tree.py
@@ -19,17 +19,20 @@ nodes = [Node(i) for i in range(1, n + 1)]
 for e1, e2 in edges:
     e1, e2 = e1 - 1, e2 - 1
     nodes[e1].add_child(nodes[e2])
+    nodes[e2].add_child(nodes[e1])
 
 parents = [0] * (n + 1)
 
 # DFS
 root = nodes[0]
-stk = [root]
+stk = [(root, 0)]
 while stk:
-    node = stk.pop()
+    node, parent = stk.pop()
 
     for adj in node.children:
+        if adj.num == parent:
+            continue
         parents[adj.num] = node.num
-        stk.append(adj)
+        stk.append((adj, node.num))
 
 print(*parents[2:], sep='\n')
