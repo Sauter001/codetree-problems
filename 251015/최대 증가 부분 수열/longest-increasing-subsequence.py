a330c003259ea10e07c9883f2bef356c4f692852
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-10-15 00:47:59 +0900

    Title: 최대 증가 부분 수열, Time: 60ms, Memory: 17MB, Status: Passed - Codetree

--- 251015/최대 증가 부분 수열/longest-increasing-subsequence.py
+++ 251015/최대 증가 부분 수열/longest-increasing-subsequence.py
@@ -5,8 +5,6 @@ dp = [1] * n
 for i in range(1, n):
     for j in range(i):
         if arr[j] < arr[i]:
-            dp[i] = max(dp[i - 1], dp[j] + 1)
-        else:
-            dp[i] = dp[i - 1]
+            dp[i] = max(dp[i], dp[j] + 1)
 
 print(max(dp))
