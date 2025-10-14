563229f43f22de148ecffa681851666ecbd32606
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-10-14 18:54:47 +0900

    Title: 삼각형 컨베이어 벨트, Time: 92ms, Memory: 19MB, Status: Wrong Answer - Codetree

---
+++ 251014/삼각형 컨베이어 벨트/conveyor-belt-triangle.py
@@ -0,0 +1,26 @@
+n, t = map(int, input().split())
+
+ls = list(map(int, input().split()))
+rs = list(map(int, input().split()))
+ds = list(map(int, input().split()))
+
+def roll(arr):
+    val = arr[-1]
+    for i in range(n-1, 0, -1):
+        arr[i] = arr[i-1]
+    arr[0] = val
+
+for _ in range(t):
+    left_end, right_end, down_end = ls[-1], rs[-1], ds[-1]
+
+    roll(ls)
+    roll(rs)
+    roll(ds)
+
+    ls[0] = down_end
+    rs[0] = left_end
+    ds[0] = right_end
+
+    print(*ls)
+    print(*rs)
+    print(*ds)
