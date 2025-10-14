5decf42617de248561ccba2fe3a66d9da4d99ccc
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-10-14 19:37:21 +0900

    Title: K개 중에 1개를 N번 뽑기, Time: 208ms, Memory: 26MB, Status: Passed - Codetree

--- 251014/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
+++ 251014/K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition.py
@@ -1,15 +1,16 @@
+import sys
+sys.setrecursionlimit(10**6)
+
 k, n = map(int, input().split())
 
 def perm(k, n, arr):
     if n == 0:
         print(*arr)
+        return
     
     for i in range(1, k + 1):
-        if i in arr: 
-            continue
-
         arr.append(i)
-        perm(k, n - 1)
+        perm(k, n - 1, arr)
         arr.pop()
 
 perm(k, n, [])
