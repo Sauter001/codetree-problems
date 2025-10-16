d5cfb7a23da3e8e50f3957222c7525db945d7523
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2025-10-16 15:01:50 +0900

    Title: 최대 점프 횟수, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 251016/최대 점프 횟수/maximum-number-of-jumps.py
+++ 251016/최대 점프 횟수/maximum-number-of-jumps.py
@@ -1,6 +1,6 @@
 n = int(input())
 arr = list(map(int, input().split()))
-dp = [-1] * n
+dp = [-1000] * n
 dp[0] = 0
 
 for i in range(1, n):
@@ -8,4 +8,4 @@ for i in range(1, n):
         if arr[j] + j >= i:
             dp[i] = max(dp[i], dp[j] + 1)
 
-print(max(dp))
+print(dp[-1])
