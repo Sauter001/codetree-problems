3c657e4047c9a821124810a37536e86de8464212
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2026-01-23 14:06:41 +0900

    Title: 밭의 높이를 고르게하기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 260123/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 260123/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -3,12 +3,15 @@ arr = list(map(int, input().split()))
 diff_arr = sorted([abs(x - height) for x in arr], reverse=True)
 
 count = 0
-for i in range(trial):
+trial_count = len(list(filter(lambda d: d == 0, diff_arr)))
+
+while trial_count <= trial:
+    trial_count += 1
     if not diff_arr:
         break
 
     diff = diff_arr.pop()
     if diff != 0:
         count += diff
-
+    
 print(count)
