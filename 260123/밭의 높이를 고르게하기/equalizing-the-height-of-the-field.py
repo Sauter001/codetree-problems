a6ae90ab4c93cf97eb12054699098a9756da433d
Sauter001 <54736876+Sauter001@users.noreply.github.com>
2026-01-23 14:15:48 +0900

    Title: 밭의 높이를 고르게하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 260123/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 260123/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -1,6 +1,6 @@
 n, height, trial = map(int, input().split())
 arr = list(map(int, input().split()))
-diff_arr = sorted([abs(x - height) for x in arr], reverse=True)
+diff_arr = [abs(x - height) for x in arr]
 diff_prefix = [0] * (n + 1)
 
 s = 0
@@ -8,9 +8,8 @@ for i in range(n):
     s += diff_arr[i]
     diff_prefix[i + 1] = s
 
-# print(diff_prefix)
 min_cost = float('inf')
 for i in range(n - trial + 1):
-    min_cost = min(min_cost, diff_prefix[i + trial - 1] - diff_prefix[i])
+    min_cost = min(min_cost, diff_prefix[i + trial] - diff_prefix[i])
 
 print(min_cost)
